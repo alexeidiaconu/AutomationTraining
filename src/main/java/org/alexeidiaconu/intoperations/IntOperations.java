ebe9c14379c34ffbebfba340edf460b0e4d04f9a
alexdiaconu <alexei.diaconu@endava.com>
2025-07-01 16:31:22 +0300

    added Biggest Number method

---
+++ src/main/java/org/alexeidiaconu/intoperations/IntOperations.java
@@ -0,0 +1,13 @@
+package org.alexeidiaconu.intoperations;
+
+public class IntOperations {
+
+    public int biggestNumber (int a, int b) {
+        if (a > b) {
+            return a;
+        }
+        else {
+            return b;
+        }
+    }
+}
